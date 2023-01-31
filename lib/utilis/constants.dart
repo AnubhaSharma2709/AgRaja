bcfe59c08c492095a123a1475d66751f5d2dcb54
AnubhaSharma2709 <anubhaa.sharma27@gmail.com>
2023-02-01 01:11:38 +0530

    commit

--- lib/utilis/constants.dart
+++ lib/utilis/constants.dart
@@ -3,12 +3,15 @@ const loginText = "Login";
 const registerText = "Register";
 const subText1 = "Don't have an account?";
 const subText2 = "Have an account? ";
+const logoutText = "Logout";
+const subText3 = "Take me to the Online Purchase Store";
 
 //Dail Emergency Contacts
 const number1 = "Dail Ambulance";
 const number2 = "Dail Emergency Contact";
 const number3 = "Dail Women Helpline Number";
 const number4 = "Dail AutoMechanic";
+const number5 = "Add Emergency Contact";
 
 //Nearest Locations Near Me
 const location1 = "Nearest Police Station";
@@ -19,6 +22,8 @@ const location4 = "Nearest NGO for Shelter Home";
 //Sheild Mode
 const safeText = "Sheild Mode will Enable in 15 mins. We will send your location to the nearby contact";
 
+
 //Community
 //Online Store
 //Other Misc Text
+//AI Chatbot Implementation
