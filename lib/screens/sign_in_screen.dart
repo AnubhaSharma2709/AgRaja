d4e4be63b9ea1eb33bfbcf5cb75562554fc7c4fc
AnubhaSharma2709 <anubhaa.sharma27@gmail.com>
2023-02-03 00:41:14 +0530

    commit

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -26,73 +26,75 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     Size screenSize = utilis().getscreenSize();
     return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 10,
-          vertical: 20,
-        ),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      'lib/assets/logo.jpeg',
-                      height: screenSize.height * 0.25,
-                      width: screenSize.width * 0.70,
-                    ),
-                    Container(
-                      height: screenSize.height * 0.5,
-                      width: screenSize.height * 0.5,
-                      padding: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: outColor,
-                          width: 2.5,
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 30,
+          ),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'lib/assets/logo.jpeg',
+                        height: screenSize.height * 0.25,
+                        width: screenSize.width * 0.70,
+                      ),
+                      Container(
+                        height: screenSize.height * 0.5,
+                        width: screenSize.height * 0.5,
+                        padding: EdgeInsets.all(15.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: outColor,
+                            width: 2.5,
+                          ),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Center(
+                                child: Text(
+                              loginText,
+                              style: TextStyle(
+                                  fontSize: 28, fontWeight: FontWeight.bold),
+                            )),
+                            TextFieldWidget(
+                              title: emailText,
+                              controller: emailController,
+                              obscureText: false,
+                              hintText: HintText1,
+                            ),
+                            TextFieldWidget(
+                              title: passText,
+                              controller: passController,
+                              obscureText: false,
+                              hintText: HintText2,
+                            ),
+                            Center(
+                              child: MainButton(
+                                  child: Text(loginText),
+                                  color: boxColor,
+                                  isLoading: false,
+                                  onPressed: (){}
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Center(
-                              child: Text(
-                            loginText,
-                            style: TextStyle(
-                                fontSize: 28, fontWeight: FontWeight.bold),
-                          )),
-                          TextFieldWidget(
-                            title: emailText,
-                            controller: emailController,
-                            obscureText: false,
-                            hintText: HintText1,
-                          ),
-                          TextFieldWidget(
-                            title: passText,
-                            controller: passController,
-                            obscureText: false,
-                            hintText: HintText2,
-                          ),
-                          Center(
-                            child: MainButton(
-                                child: Text(loginText),
-                                color: boxColor,
-                                isLoading: false,
-                                onPressed: (){}
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
