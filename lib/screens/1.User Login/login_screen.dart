17c15ed7770e24371d4687e63c1cb583ef4361f3
AnubhaSharma2709 <anubhaa.sharma27@gmail.com>
2023-02-21 23:56:38 +0530

    commit

--- lib/screens/1.User Login/login_screen.dart
+++ lib/screens/1.User Login/login_screen.dart
@@ -11,6 +11,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+
 class SignInScreen extends StatefulWidget {
   const SignInScreen({Key? key}) : super(key: key);
 
@@ -29,112 +30,135 @@ class _SignInScreenState extends State<SignInScreen> {
   Widget build(BuildContext context) {
     Size screenSize = utilis().getscreenSize();
     return Scaffold(
-      backgroundColor: backgroundColor,
-      body: _isLoading ? Center(
-        child: CircularProgressIndicator(
-          color: colorBorder,),
-      ) : SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 10,
-            vertical: 30,
-          ),
-          child:Column(
-            children:<Widget>[
-            Image.asset(
-              'lib/assets/logo.jpeg',
-              height: screenSize.height * 0.2,
-              width: screenSize.width * 0.5,
-            ),
-            Form(
-              key: formKey,
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    //mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                                        Text(loginText, style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: colorBorder,
+        body: _isLoading
+            ? Center(
+                child: CircularProgressIndicator(
+                  color: colorBorder,
+                ),
+              )
+            : SingleChildScrollView(
+                  child: Container(
+                     // height: MediaQuery.of(context).size.height,
+                     // width: MediaQuery.of(context).size.width,
+                     // decoration: BoxDecoration(
+                     //   image: DecorationImage(
+                     //     image: AssetImage('lib/assets/background.png'),
+                     //     fit: BoxFit.fitHeight,
+                     //   ),
+                     // ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 30,
                       ),
-                      ),
-                      SizedBox(height: screenSize.height * 0.05,),
-                      TextFormField(
-                        decoration: textInputDecoration.copyWith(
-                          labelText: emailText,
-                          hintText: hintEmailText,
-                          prefixIcon: Icon(Icons.email, color: outlineBorder),
-                        ),
-                        onChanged: (val) {
-                          setState(() {
-                            email = val;
-                            print(email);
-                          });
-                        },
-                      ),
-                      SizedBox(height: screenSize.height * 0.05,),
-                      TextFormField(
-                        decoration: textInputDecoration.copyWith(
-                          labelText: passText,
-                          hintText: hintPassText,
-                          prefixIcon: Icon(Icons.key, color: outlineBorder),
-                        ),
-                        validator: (val) {
-                          if (val!.length < 6) {
-                            return "The password incorrect";
-                          } else {
-                            return null;
-                          }
-                        },
-                        onChanged: (val) {
-                          setState(() {
-                            password = val;
-                          });
-                        },
-                      ),
-                      SizedBox(height: screenSize.height * 0.05,),
-                      Center(
-                        child: MainButton(
-                            child: Text(loginText),
-                            color: outlineBorder,
-                            isLoading: false,
-                            onPressed: () {
-                              login();
-                            }),
-                      ),
-                      Text.rich(TextSpan(
-                        text: newText,
-                        style: const TextStyle(
-                            color: Colors.black,
-                            fontSize: 14),
-                        children: <TextSpan>[
-                          TextSpan(
-                            text: registerText,
-                            style: const TextStyle(
-                                color: spanColor,
-                                decoration: TextDecoration.underline
+                      child: Column(
+                        children: <Widget>[
+                          Image.asset(
+                            'lib/assets/logo.jpeg',
+                            height: MediaQuery.of(context).size.height * 0.19,
+                            width: MediaQuery.of(context).size.width * 0.75,
+                          ),
+                          Form(
+                            key: formKey,
+                            child: Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  //mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: <Widget>[
+                                    Text(
+                                      loginText,
+                                      style: TextStyle(
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold,
+                                        color: colorBorder,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: screenSize.height * 0.05,
+                                    ),
+                                    TextFormField(
+                                      decoration: textInputDecoration.copyWith(
+                                        labelText: emailText,
+                                        hintText: hintEmailText,
+                                        prefixIcon: Icon(Icons.email,
+                                            color: outlineBorder),
+                                      ),
+                                      onChanged: (val) {
+                                        setState(() {
+                                          email = val;
+                                          print(email);
+                                        });
+                                      },
+                                    ),
+                                    SizedBox(
+                                      height: screenSize.height * 0.05,
+                                    ),
+                                    TextFormField(
+                                      decoration: textInputDecoration.copyWith(
+                                        labelText: passText,
+                                        hintText: hintPassText,
+                                        prefixIcon:
+                                            Icon(Icons.key, color: outlineBorder),
+                                      ),
+                                      validator: (val) {
+                                        if (val!.length < 6) {
+                                          return "The password incorrect";
+                                        } else {
+                                          return null;
+                                        }
+                                      },
+                                      onChanged: (val) {
+                                        setState(() {
+                                          password = val;
+                                        });
+                                      },
+                                    ),
+                                    SizedBox(
+                                      height: screenSize.height * 0.05,
+                                    ),
+                                    Center(
+                                      child: MainButton(
+                                          child: Text(loginText),
+                                          color: outlineBorder,
+                                          isLoading: false,
+                                          onPressed: () {
+                                            login();
+                                          }),
+                                    ),
+                                    Text.rich(
+                                      TextSpan(
+                                        text: newText,
+                                        style: const TextStyle(
+                                            color: Colors.black, fontSize: 14),
+                                        children: <TextSpan>[
+                                          TextSpan(
+                                            text: registerText,
+                                            style: const TextStyle(
+                                                color: spanColor,
+                                                decoration:
+                                                    TextDecoration.underline),
+                                            recognizer: TapGestureRecognizer()
+                                              ..onTap = () {
+                                                nextScreen(context,
+                                                    const SignUpScreen());
+                                              },
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
                             ),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                nextScreen(context, const SignUpScreen());
-                              },
                           ),
                         ],
                       ),
-                      ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ),
-         ],
-          ),
-        ),
-      ),
-    );
+              );
   }
 
   login() async {
@@ -147,17 +171,14 @@ class _SignInScreenState extends State<SignInScreen> {
           .then((value) async {
         if (value == true) {
           QuerySnapshot snapshot =
-          await DatabaseService(
-              uid: FirebaseAuth.instance.currentUser!.uid)
-              .gettingUserData(email);
+              await DatabaseService(uid: FirebaseAuth.instance.currentUser!.uid)
+                  .gettingUserData(email);
           //saving the values to our shared preferences
           await HelperFunctions.saveUserLoggedInStatus(true);
           await HelperFunctions.saveUserEmailSF(email);
-          await HelperFunctions.saveUserNameSF(
-              snapshot.docs[0]['fullName']);
+          await HelperFunctions.saveUserNameSF(snapshot.docs[0]['fullName']);
           nextScreenReplace(context, const HomePage());
-        }
-        else {
+        } else {
           showSnackBar(context, errorBorder, value);
           setState(() {
             _isLoading = false;
